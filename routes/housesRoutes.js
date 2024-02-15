4dfd4c8bddd1636b4df50e4d771f3106f2e75aa1
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-02-15 20:32:41 +0700

    updated code with actual data

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -3,12 +3,15 @@ const router = Router()
 
 // Define a GET route for fetching the list of users
 router.get('/houses', (req, res) => {
-  res.send('List of houses')
+  res.json([
+    { id: 1, location: 'Koh Phangan' },
+    { id: 2, location: 'Bali' }
+  ])
 })
 
 // Define a GET route for fetching a single user
 router.get('/houses/1', (req, res) => {
-  res.send('User number 1')
+  res.json({ id: 1, location: 'Koh Phangan' })
 })
 
 // Export the router
