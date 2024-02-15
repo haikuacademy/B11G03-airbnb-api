fead5053f769e0a6d6689a99af832e4f649f2986
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-15 12:40:33 +0700

    added review routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import reviewsRoutes from './routes/reviewsRoutes.js'
 
 const app = express()
+app.use(reviewsRoutes)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
---
+++ routes/reviewsRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+const router = Router()
+
+router.get('/reviews', (req, res) => {
+  res.send([
+    { id: 1, rating: 3 },
+    { id: 2, rating: 4 },
+    { id: 3, rating: 5 }
+  ])
+})
+
+router.get('/reviews/1', (req, res) => {
+  res.send({ id: 1, rating: 3 })
+})
+
+export default router
