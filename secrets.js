9cf968241bc53f0ba7eb0c7f8ce1dd49ea1c342a
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-16 13:13:05 +0700

    imported db to update the GET /bookings route

---
+++ secrets.js
@@ -0,0 +1,2 @@
+export const DBURL =
+  'postgres://b11g03_airbnb_api_database_based_on_user:v5pXaTJps4OTZtzYRhsWKBkpjv7cME8n@dpg-cn7di8021fec73fl9k10-a.singapore-postgres.render.com/b11g03_airbnb_api_database_based_on'
