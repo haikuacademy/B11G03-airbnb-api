6faf323d764a80d4ac1a6a65a1312c79660e067e
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-19 13:07:10 +0700

    added params to reviews route so you can search by review id

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 const router = Router()
 
+import db from '../db.js'
+
 router.get('/reviews', (req, res) => {
   res.json([
     { id: 1, rating: 3 },
@@ -9,8 +11,19 @@ router.get('/reviews', (req, res) => {
   ])
 })
 
-router.get('/reviews/1', (req, res) => {
-  res.send({ id: 1, rating: 3 })
+router.get('/reviews/:reviewID', async (req, res) => {
+  let reviewId = req.params.userId
+  try {
+    const { rows } = await db.query(
+      `SELECT * FROM reviews WHERE review_id = ${req.params.reviewID}`
+    )
+    const returnObject =
+      rows.length > 0 ? rows[0] : { error: 'review not found' }
+    res.json(returnObject)
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
 
 export default router
