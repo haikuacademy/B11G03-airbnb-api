aa4615186824e443167a8a3b9eec6c8c77586c04
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-20 11:36:08 +0700

    Tidied up code and implemented custom error handling for houses routes

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -1,10 +1,8 @@
-// Packages
 import { Router } from 'express'
 import db from '../db.js'
 
 const router = Router()
 
-// Define a GET route for fetching the list of houses
 router.get('/houses', async (req, res) => {
   let location = req.query.location || ''
   let max_price = req.query.max_price || 10000000000
@@ -16,15 +14,12 @@ router.get('/houses', async (req, res) => {
     const { rows } = await db.query(
       `SELECT * FROM houses WHERE location iLIKE '%${location}%' AND price_per_night <= ${max_price} AND bedrooms >= ${min_rooms} AND description iLIKE '%${search}%' ORDER BY ${sort} ${order}`
     )
-    console.log(rows)
     res.json(rows)
   } catch (err) {
-    console.error(err.message)
     res.json(err)
   }
 })
 
-// Fetching a house using request params
 router.get('/houses/:houseId', async (req, res) => {
   let houseId = req.params.houseId
   try {
@@ -32,15 +27,14 @@ router.get('/houses/:houseId', async (req, res) => {
       `SELECT * FROM houses WHERE house_id = ${houseId}`
     )
     const result = rows[0]
-    console.log(result)
     if (result === undefined) {
-      res.json({ Error: 'House Not Found' })
+      throw new Error(`No house found with ID ${houseId}`)
     }
     res.json(result)
   } catch (err) {
-    console.error(err.message)
-    res.json(err)
+    console.log(err.message)
+    res.send(`Error: ${err.message}`)
   }
 })
-// Export the router
+
 export default router
