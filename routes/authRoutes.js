7744ba9b31b04fb8dc9ab49d1949d4c4a2d456d2
MarieBaxter <146040378+MarieBaxter@users.noreply.github.com>
2024-02-20 14:33:34 +0700

    added POST to login route so that users can log in by matching their email and password with what's stored in the database

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,27 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/signup', (req, res) => {
-  res.send('Hello from signup')
+import db from '../db.js'
+
+router.post('/login', async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  try {
+    const { rows } = await db.query(`
+      SELECT * FROM users WHERE email = '${email}' AND password = '${password}'
+    `)
+    if (rows.length === 0) {
+      throw new Error('User with that email or password does not exist.')
+    }
+    res.json({ rows })
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
 })
 
-router.get('/login', (req, res) => {
-  res.send('Hello from login')
+router.get('/signup', (req, res) => {
+  res.send('Hello from signup')
 })
 
 router.get('/logout', (req, res) => {
