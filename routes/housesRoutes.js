7f40b54d0a8531419b5a0eb73b28aab8870f5f02
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-23 14:14:34 +0700

    Implemented token check for DELETE route

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -121,6 +121,15 @@ router.patch('/houses/:houseId', async (req, res) => {
 router.delete('/houses/:houseId', async (req, res) => {
   let houseId = req.params.houseId
   try {
+    const token = req.cookies.jwt
+    if (!token) {
+      throw new Error('No token provided')
+    } else {
+      const decodedToken = jwt.verify(token, jwtSecret)
+      if (!decodedToken) {
+        throw new Error('Invalid authentication token')
+      }
+    }
     const readRows = await db.query(
       `SELECT * FROM houses WHERE house_id = ${houseId}`
     )
